7f1b46134195959280f0f3cbfe50e8f53a2ab2e7
Yulia Elagina <julia-elagina@yandex.ru>
2020-04-18 19:37:09 +0300

    Создает абстрактный класс для компонентов

---
+++ src/components/abstract-component.js
@@ -0,0 +1,24 @@
+import {createElement} from "../utils.js";
+export default class AbstractComponent {
+  constructor() {
+    if (new.target === AbstractComponent) {
+      throw new Error(`Can't instantiate AbstractComponent, only a concrete one.`);
+    }
+    this._element = null;
+  }
+
+  getTemplate() {
+    throw new Error(`Abstract method is not implemented: getTemplate`);
+  }
+
+  getElement() {
+    if (!this._element) {
+      this._element = createElement(this.getTemplate());
+    }
+    return this._element;
+  }
+
+  removeElement() {
+    this._element = null;
+  }
+}
