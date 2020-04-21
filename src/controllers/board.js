503e33d29ada55aae2a9f19093e298b1e05f1ca3
Yulia Elagina <julia-elagina@yandex.ru>
2020-04-21 23:40:44 +0300

    Переносит функцию renderLoadMore и обработчик смены типа сортировки в методы контроллера board

--- src/controllers/board.js
+++ src/controllers/board.js
@@ -18,59 +18,64 @@ const renderTasks = (taskListElement, tasks) => {
 };
 export default class Board {
   constructor(container) {
+    this._tasks = [];
     this._container = container;
+    this._showingTasksCount = SHOWING_TASKS_AMOUNT_ON_START;
 
     this._noTaskComponent = new NoTaskComponent();
     this._sortComponent = new SortComponent();
     this._tasksComponent = new TasksComponent();
     this._loadMoreButtonComponent = new LoadMoreButtonComponent();
+    // Обработчик изменения типа сортировки
+    this._onSortTypeChange = this._onSortTypeChange.bind(this);
+    this._sortComponent.setSortTypeChangeHandler(this._onSortTypeChange);
+  }
+
+  _renderLoadMore() {
+    if (this._showingTasksCount >= this._tasks.length) {
+      return;
+    }
+    render(this._container, this._loadMoreButtonComponent);
+
+    this._loadMoreButtonComponent.setClickHandler(() => {
+      const prevTasksCount = this._showingTasksCount;
+      this._showingTasksCount += SHOWING_TASKS_AMOUNT_BY_BUTTON;
+      const taskListElement = this._tasksComponent.getElement();
+      const sortedTasks = getSortedTasks(this._tasks, this._sortComponent.getSortType())
+        .slice(prevTasksCount, this._showingTasksCount);
+
+      renderTasks(taskListElement, sortedTasks);
+
+      if (this._showingTasksCount >= this._tasks.length) {
+        remove(this._loadMoreButtonComponent);
+      }
+    });
+  }
+
+  _onSortTypeChange(sortType) {
+    const taskListElement = this._tasksComponent.getElement();
+    taskListElement.innerHTML = ``;
+    this._showingTasksCount = SHOWING_TASKS_AMOUNT_ON_START;
+    const sortedTasks = getSortedTasks(this._tasks, sortType);
+    renderTasks(taskListElement, sortedTasks.slice(0, this._showingTasksCount));
+    this._renderLoadMore();
   }
 
   render(tasks) {
-    let showingTasks = tasks.slice();
-    const renderLoadMore = () => {
-      render(container, this._loadMoreButtonComponent);
-
-      this._loadMoreButtonComponent.setClickHandler(() => {
-        const prevTasksCount = showingTasksCount;
-        showingTasksCount += SHOWING_TASKS_AMOUNT_BY_BUTTON;
-        showingTasks.slice(prevTasksCount, showingTasksCount)
-          .forEach((task) => renderTask(taskListElement, task));
-        if (showingTasksCount >= showingTasks.length) {
-          remove(this._loadMoreButtonComponent);
-        }
-      });
-    };
-
+    this._tasks = tasks;
     const container = this._container.getElement();
-    const isAllTasksArchived = tasks.every((task) => task.isArchive);
+    const isAllTasksArchived = this._tasks.every((task) => task.isArchive);
     if (isAllTasksArchived) {
       render(container, this._noTaskComponent);
       return;
     }
 
-    // Отрисовываю блок сортировки
     render(container, this._sortComponent);
-
-    // Отрисовываю контейнер для карточек задач
     render(container, this._tasksComponent);
 
-    const taskListElement = container.querySelector(`.board__tasks`);
-    // Отрисовываю первую порцию карточек
-    let showingTasksCount = SHOWING_TASKS_AMOUNT_ON_START;
-    renderTasks(taskListElement, showingTasks.slice(0, showingTasksCount));
+    const taskListElement = this._tasksComponent.getElement();
+    renderTasks(taskListElement, this._tasks.slice(0, this._showingTasksCount));
 
-    // Кнопка Load More c обработчиком
-    renderLoadMore();
-
-    // Навешиваю обработчик клика на блок сортировки
-    this._sortComponent.setSortTypeChangeHandler((sortType) => {
-      // Удалить задачи с доски, запустить фильтр, отрисовать новые карточки задач
-      taskListElement.innerHTML = ``;
-      showingTasksCount = SHOWING_TASKS_AMOUNT_ON_START;
-      showingTasks = getSortedTasks(tasks, sortType);
-      renderTasks(taskListElement, showingTasks.slice(0, showingTasksCount));
-      renderLoadMore();
-    });
+    this._renderLoadMore();
   }
 }
