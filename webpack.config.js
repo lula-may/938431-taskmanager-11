d09a94f0f440ccaff5dd2aef58bc8d1fff32d1ac
Yulia Elagina <julia-elagina@yandex.ru>
2020-04-28 22:16:25 +0300

    Переустанавливает точные версии пакетов flatpickr & moment, добавляет локаль в плагин

--- webpack.config.js
+++ webpack.config.js
@@ -6,22 +6,24 @@ module.exports = {
   entry: `./src/main.js`,
   output: {
     filename: `bundle.js`,
-    path: path.join(__dirname, `public`)
+    path: path.join(__dirname, `public`),
   },
   devtool: `source-map`,
   devServer: {
     contentBase: path.join(__dirname, `public`),
-    watchContentBase: true
+    watchContentBase: true,
   },
   module: {
     rules: [
       {
         test: /\.css$/i,
-        use: [`style-loader`, `css-loader`],
+        use: [`style-loader`, `css-loader`]
       },
     ],
   },
   plugins: [
-    new MomentLocalesPlugin(),
-  ]
+    new MomentLocalesPlugin({
+      localesToKeep: [`es-us`],
+    })
+  ],
 };
