d2c13fd15dc0aa3fcf6662577272af1be61c0d57
Yulia Elagina <julia-elagina@yandex.ru>
2020-04-29 23:45:59 +0300

    Создает контроллер фильтра

---
+++ src/controllers/filter.js
@@ -0,0 +1,45 @@
+import FilterComponent from "../components/filter.js";
+import {FilterType} from "../const.js";
+import {getTasksByFilter} from "../utils/filter.js";
+import {render, replace} from "../utils/render.js";
+
+export default class FilterController {
+  constructor(container, tasksModel) {
+    this._container = container;
+    this._tasksModel = tasksModel;
+    this._activeFilterType = FilterType.ALL;
+    this._filterComponent = null;
+
+    this._onFilterChange = this._onFilterChange.bind(this);
+
+  }
+
+  render() {
+    const allTasks = this._tasksModel.getTasks();
+    const filters = this._getFilters(allTasks);
+    const oldComponent = this._filterComponent;
+    this._filterComponent = new FilterComponent(filters);
+    this._filterComponent.setFilterChangeHandler(this._onFilterChange);
+
+    if (oldComponent) {
+      replace(this._filterComponent, oldComponent);
+    } else {
+      render(this._container, this._filterComponent);
+    }
+  }
+
+  _getFilters(tasks) {
+    return Object.values(FilterType).map((name) => {
+      return {
+        name,
+        count: getTasksByFilter(tasks, name).length,
+        checked: name === this._activeFilterType,
+      };
+    });
+  }
+
+  _onFilterChange(filterType) {
+    this._tasksModel.setFilter(filterType);
+    this._activeFilterType = filterType;
+  }
+}
--- src/main.js
+++ src/main.js
@@ -1,24 +1,27 @@
 import BoardComponent from "./components/board.js";
 import BoardController from "./controllers/board.js";
-import FilterComponent from "./components/filter.js";
+import FilterController from "./controllers/filter.js";
+// import FilterComponent from "./components/filter.js";
 import MainMenuComponent from "./components/main-menu.js";
 import TasksModel from "./models/tasks.js";
 import {generateTasks} from "./mock/task.js";
-import {generateFilters} from "./mock/filter.js";
+// import {generateFilters} from "./utils/filter.js";
 import {render} from "./utils/render.js";
 
 const TASK_AMOUNT = 20;
 const tasks = generateTasks(TASK_AMOUNT);
 const tasksModel = new TasksModel();
 tasksModel.setTasks(tasks);
-const filters = generateFilters(tasks);
+// const filters = generateFilters(tasks);
 
 const siteMainElement = document.querySelector(`.main`);
 const siteMenuElement = siteMainElement.querySelector(`.main__control`);
 const boardComponent = new BoardComponent();
 
 render(siteMenuElement, new MainMenuComponent());
-render(siteMainElement, new FilterComponent(filters));
+const filterController = new FilterController(siteMainElement, tasksModel);
+filterController.render();
+// render(siteMainElement, new FilterComponent(filters));
 render(siteMainElement, boardComponent);
 const boardController = new BoardController(boardComponent, tasksModel);
 boardController.render(tasks);
